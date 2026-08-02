6428a59c0ccbc5dfdd07dfc814667b104cdf2678
Ahmed Omar <technestagency@gmail.com>
2026-08-02 16:09:03 +0300

    Revert "Revert "Exclude XPay webhook from CSRF in bootstrap/app.php (Laravel 11)""
    This reverts commit deadc800fb2886b3d1562c0f283d4ad483476bfd.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->validateCsrfTokens(except: [
+            'payment/xpay/webhook',
             'payment/xpay/callback',
             'api/handle-anubpay-payment',
         ]);
